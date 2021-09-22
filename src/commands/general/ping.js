6efba8888d888b91474a2597378450e8f4a6d596
Alex <47509591+alexbarlim@users.noreply.github.com>
2021-09-22 15:41:14 -0300

    Update ping.js

--- src/commands/general/ping.js
+++ src/commands/general/ping.js
@@ -2,11 +2,11 @@ const Discord = require("discord.js");
 
 module.exports = {
     name: "ping",
-    description: "Pings the server",
+    description: "Pings para o servidor",
     async execute(message, args) {
         const msg = await message.channel.send("Ping?");
         msg.edit(
-            `:ping_pong: **Pong!** Took \`${
+            `:ping_pong: **Pong!** retornou \`${
                 msg.createdTimestamp - message.createdTimestamp
             }ms\``
         );

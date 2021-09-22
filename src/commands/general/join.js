e7cda2be75c250ef74ccea6e61dbcda85e7a9a7d
Alex <47509591+alexbarlim@users.noreply.github.com>
2021-09-22 15:39:20 -0300

    Update join.js

--- src/commands/general/join.js
+++ src/commands/general/join.js
@@ -2,15 +2,15 @@ const Discord = require("discord.js");
 
 module.exports = {
     name: "join",
-    description: "Connects the bot to your voice channel",
+    description: "Conecta o bot ao seu canal de voz",
     async execute(message, args) {
         if (!message.member.hasPermission("CONNECT"))
-            return message.reply("You do not have the permissions to do that");
+            return message.reply("Você não tem permissão para fazer isso");
 
         if (message.member.voice.channel) {
             connection = await message.member.voice.channel.join();
         } else {
-            message.reply("You have to be in a voice channel to play music");
+            message.reply("Você tem que estar em um canal de voz para tocar música");
         }
     },
 };
